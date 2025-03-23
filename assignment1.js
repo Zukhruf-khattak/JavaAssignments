f891eb836f67858976edf22c58695de3f4f94434
Zukhruf-khattak <itszohroufkhattak@gmail.com>
2025-03-23 11:26:56 +0500

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,61 @@
+// let age = prompt(" Enter Your Age Please!");
+// age = parseInt(age)
+
+// if ( age >="18" ){
+//     console.log("You have a driving license, You can drive.")
+// }
+// else if ( age <"18"){
+//     console.log("You are younger than 18, You are too young to drive.")
+// }
+
+//  else{
+//     console.log(" You need a license to drive.")
+//  }
+
+
+// ---------------------------------------------------------->
+
+
+// let num = prompt(" Enter Two number Please!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------->
+
+
+// let colors = ["Black", "Red", "Golden" , "White" , "Purple"]
+//  console.log(colors)
+
+//  let index = parseInt(prompt("Enter an index number from 0 to 4 :"))
+//  console.log(colors)
+
+
+// ------------------------------------------------------------------>
+
+
+// let array = ["1","2","3","4"]
+// console.log(array)
+
+// array.push("5")
+// console.log(array)
+
+// array.pop("5")
+// console.log(array)
+
+// ----------------------------------------------------------------------->

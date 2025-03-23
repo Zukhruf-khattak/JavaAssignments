8070007f776ec6de3b9e20429e0ee6b80b8c5875
Zukhruf-khattak <itszohroufkhattak@gmail.com>
2025-03-23 11:27:39 +0500

    Add files via upload

---
+++ assig3.js
@@ -0,0 +1,35 @@
+// JavaScript Assignment
+// Chap:19  Flags, Booleans, Array Length, and Loop Interruptions
+// (use flags and booleans)
+//  Q1: Declare a Boolean variable isFound and set it to false. Write a loop to search for the number 7. If found, update isFound to true and break the loop.
+
+
+
+
+
+//  Q2: Write a program that takes a list of numbers and checks if all numbers are positive. Use a flag to store the result and display "All Positive" or "Contains Negative".
+// (Array Length in Loops)
+// Q4: Declare an array with 10 random numbers. Use a loop to iterate through the array using its length property and print each number.
+// Q5: Declare an empty array. Ask the user to enter 5 numbers one by one and store them in the array. Use a loop to display the numbers along with the total length of the array.
+// Q6: Create an array of your five favorite books. Use a loop to display each book name along with its index.
+
+
+// (Loopus Interruptus)
+//  Q7: Write a loop that prints numbers from 1 to 20 but skips multiples of 3 using the continue statement.
+// Q8: Create an array of exam scores [45, 78, 90, 55, 32, 88]. Use a loop to find the first failing score (less than 40) and break the loop once found.
+// Chap:20 (Nested Loop)
+// Q1:Use a nested loop which will create Or make a triangle(use numbers)
+// Q2: Write a program that uses nested loops to generate a multiplication table for numbers 1 to 4. The table should multiply each number by values from 1 to 5
+
+
+// Chap:21 (changing case)
+// Q1: Write a JavaScript program that takes a user’s name as input and converts it to uppercase.
+// Q2: Create an array of five random words in lowercase. Use a loop to convert each word to uppercase and print the updated array.
+
+
+// Chap:26 (Rounding numbers)
+// Q1: Write a  program that takes a decimal number as input and rounds it to the nearest whole number using Math.round().
+// Q2: Create an array of five decimal numbers. Use a loop to apply Math.round() to each number and print the rounded values.
+// Q3: Round a decimal number up to the next whole number using Math.ceil()
+// Q4: Round a decimal number down to the nearest integer using Math.floor()
+
